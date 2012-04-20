119cf0c68b40cafd8c2e42ccb457a3b3b2a3dd4a
SteveJohnson <steve@rewardme.com>
2012-04-19 19:00:50 -0700

    template;

---
+++ Conquer-iOS/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  Conquer-iOS
+//
+//  Created by Mac User on 4/19/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ Conquer-iOS/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Conquer-iOS
+//
+//  Created by Mac User on 4/19/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
